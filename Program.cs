c991579f63675bcb605d72a358113143c42f8649
Brooke Kullberg and Justin Kerntz <brooke@>
2019-04-22 16:19:29 -0700

    add finished code to Palindrome method

--- Program.cs
+++ Program.cs
@@ -2,37 +2,32 @@ using System;
 using System.Collections.Generic;
 
 public class Program
+{
+  public static void Main()
   {
-    public static void Main()
+
+    Console.WriteLine("Enter a word to see if it is a Palindrome:");
+    string userInput = Console.ReadLine();
+    string backwardsUserInput = "";
+
+    for (int i = 0; i < userInput.Length; i++)
     {
-
-      Console.WriteLine("Enter a word to see if it is a Palindrome:");
-      string userInput = Console.ReadLine();
-      string backwardsUserInput = "";
-
-      for (int i = 0; i < userInput.Length; i++)
+      if (userInput[userInput.Length -i -1] != userInput[i])
       {
-        if (userInput[userInput.Length -i -1] != userInput[i])
-        {
-          System.Console.Write("Not a palindrome!");
-          i += userInput.Length;
-        }
-        else if (userInput[userInput.Length -i -1] == userInput[i])
-        {
-          backwardsUserInput += userInput[i];
-        }
+        Console.WriteLine("Not a palindrome!");
+        i += userInput.Length;
       }
+      else if (userInput[userInput.Length -i -1] == userInput[i])
+      {
+        backwardsUserInput += userInput[i];
+      }
+    }
 
-      System.Console.Write(backwardsUserInput);
-
-      // for (int i = 0; i < userInput.Length; i++)
-      // {
-      //   System.Console.Write(userInput[userInput.Length -i -1]);
-      //
-      //   if (userInput[userInput.Length -i -1] == userInput[i])
-      //   {
-      //     Console.WriteLine("You made it");
-      //   }
-      // }
+    if (backwardsUserInput.Length == userInput.Length)
+    {
+      Console.WriteLine("Congratulations, this is a Palindrome: ");
+      Console.WriteLine(userInput + " is " + backwardsUserInput + " backwards. Now ya Know.");
     }
   }
+
+}
